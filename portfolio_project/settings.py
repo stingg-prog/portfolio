9f7f146f4eb5906c1eb78e86889c043660d75e5c
Syamraj <syamrajalpy334@gmail.com>
2026-06-12 09:51:47 +0530

    Update settings for deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=syamraj-super-developer-security-token-key='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
 
 
 # Application definition
